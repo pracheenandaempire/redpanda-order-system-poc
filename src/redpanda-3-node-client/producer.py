9418b2e1b37c2cf2199930ffc86080f0904fbe1d
matthewzhangempire <matthew.zhang@empire.ca>
2024-08-19 11:51:09 -0400

    Running Producer container

--- src/redpanda-3-node-client/producer.py
+++ src/redpanda-3-node-client/producer.py
@@ -56,8 +56,8 @@ if __name__ == "__main__":
   logging_config.configure_logging()
   
   topic = "orders"
-  bootstrap_servers = ["localhost:19092", "localhost:29092", "localhost:39092",]
-  
+  bootstrap_servers = ["redpanda-0:19092", "redpanda-1:29092", "redpanda-2:39092",]
+  logging.info(f"{bootstrap_servers}")
   admin = OrderAdmin(bootstrap_servers)
   producer = OrderProducer(bootstrap_servers, topic)
   admin.create_topic(topic)
